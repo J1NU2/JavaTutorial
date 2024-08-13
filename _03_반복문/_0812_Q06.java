2c6f138d59821f06b7c0654796f38b367fbf3376
J1NU2 <pj1nu2@gmail.com>
2024-08-13 11:24:07 +0900

    Java 조건문 미션

--- _03_반복문/_0812_Q06.java
+++ _03_반복문/_0812_Q06.java
@@ -10,20 +10,61 @@ public class _0812_Q06 {
 		// 메서드는 최대한 적게 사용하시오.
 		String a ="23-56+45*2-56";	// 초기값
 		
-		String bT = "";
-		String nT = "";
-		String arithmetic = "";
-		String result = "";
-		for (int i=0; i<a.length(); i++) {
+		String text = "";			// 문자열 a에서 숫자를 담을 변수
+		char arithmetic = '?';		// 문자열 a에서 연산자를 담을 변수
+		int result = 0;				// 문자열 a의 사칙연산 최종값을 담을 변수
+		for (int i=0; i<a.length(); i++) {		// 0부터 문자열 a의 마지막 인덱스까지 반복
+			// ASCII Code에서 47을 기준으로 47이 넘어가면(48~) 0 1 2 3 4 ... 등의 숫자를
+			// 47 이하라면 / . - , + * ... 등의 사칙연산자가 나오게 된다.
+			// 문자열 a의 i번째의 문자가 47보다 클 경우의 조건(만약 숫자라면)
 			if ((int)a.charAt(i) > 47) {
-				nT += a.charAt(i);
+				text += a.charAt(i);	// 문자열 변수인 text에 문자열 a의 i번째 문자를 추가한다.
 			} else {
-				arithmetic += a.charAt(i);
-				bT = nT;
-				nT = "";
+				// 1. result 연산자= Integer.parseInt(text); : 
+				// > text가 문자열이므로 형변환을 위해 parseInt를 사용하여 정수형으로 파싱한 뒤 result 변수에 사칙연산을 진행
+				// 2. text = ""; : 사칙연산을 진행한 뒤 숫자가 담겨져 있는 문자열 text의 값을 초기화
+				// 3. arithmetic = '?'; : 사칙연산을 진행한 뒤 연산자가 담겨져 있는 문자열 arithmetic의 값을 초기화
+				if (result == 0) {	// 맨 처음 숫자 다음으로 연산자가 나온 경우의 조건 (a문자열의 23- 부분)
+					result += Integer.parseInt(text);
+					text = "";
+				} else if (arithmetic == '+') {			// arithmetic의 문자가 + 일 경우의 조건
+					result += Integer.parseInt(text);
+					text = "";
+					arithmetic = '?';
+				} else if (arithmetic == '-') {			// arithmetic의 문자가 - 일 경우의 조건
+					result -= Integer.parseInt(text);
+					text = "";
+					arithmetic = '?';
+				} else if (arithmetic == '*') {			// arithmetic의 문자가 * 일 경우의 조건
+					result *= Integer.parseInt(text);
+					text = "";
+					arithmetic = '?';
+				} else if (arithmetic == '/') {			// arithmetic의 문자가 / 일 경우의 조건
+					result /= Integer.parseInt(text);
+					text = "";
+					arithmetic = '?';
+				} else if (arithmetic == '%') {			// arithmetic의 문자가 % 일 경우의 조건
+					result %= Integer.parseInt(text);
+					text = "";
+					arithmetic = '?';
+				}
+				arithmetic = a.charAt(i);				// arithmetic를 a문자열의 i번째 사칙연산자로 변경
 			}
+		}	// for반복문 종료
+		
+		// 마지막 값을 계산하기 위한 조건문
+		if (arithmetic == '+') {				// arithmetic의 문자가 + 일 경우의 조건
+			result += Integer.parseInt(text);
+		} else if (arithmetic == '-') {			// arithmetic의 문자가 - 일 경우의 조건
+			result -= Integer.parseInt(text);
+		} else if (arithmetic == '*') {			// arithmetic의 문자가 * 일 경우의 조건
+			result *= Integer.parseInt(text);
+		} else if (arithmetic == '/') {			// arithmetic의 문자가 / 일 경우의 조건
+			result /= Integer.parseInt(text);
+		} else if (arithmetic == '%') {			// arithmetic의 문자가 % 일 경우의 조건
+			result %= Integer.parseInt(text);
 		}
-		System.out.println(nT);
+		System.out.println(result);				// 최종값 출력
 	}
 
 }
---
+++ _03_반복문/_0812_QS1.java
@@ -0,0 +1,66 @@
+package _03_반복문;
+
+public class _0812_QS1 {
+
+	public static void main(String[] args) {
+		// 특별1
+		// 문자열 a는 한자릿수의 숫자를 수집한 문자열이다.
+		// 숫자별로 수집된 수만큼 카운팅하세요.
+		// 카운팅 배열은 int[] a = new int[10];
+		// 위 선언문은 길이 10의 배열을 생성한 선언문이다. 초기값은 0이다.
+		// 인덱스 0~9까지를 숫자로 정의한다.
+		// 조건 : 형변환은 사용하지 않는다. 메서드는 charAt()만 사용한다.
+		String aStr = "6/4/3/2/5/1/4/5/6/7/8/9/1/2/3/4/5/6";	// 초기값
+		int[] aArr = new int[10];								// 초기값
+		
+		// 1. for문 반복 : 자료형이 String인 aStr의 0번~마지막 인덱스 까지 반복
+		// 2. if문 조건 : 자료형이 aStr인 문자열에서 charAt메소드로 떼어낸 문자가 '0'~'9'에 해당될 경우의 조건
+		// > '0' 이라면 배열 aArr의 0번 인덱스의 값을 +1 (aArr[-]++)
+		for (int i=0; i<aStr.length(); i++) {	// ASCII Code 10진수
+			if (aStr.charAt(i) == '0') {		// 48
+				aArr[0]++;
+			} else if (aStr.charAt(i) == '1') {	// 49
+				aArr[1]++;
+			} else if (aStr.charAt(i) == '2') {	// 50
+				aArr[2]++;
+			} else if (aStr.charAt(i) == '3') {	// 51
+				aArr[3]++;
+			} else if (aStr.charAt(i) == '4') {	// 52
+				aArr[4]++;
+			} else if (aStr.charAt(i) == '5') {	// 53
+				aArr[5]++;
+			} else if (aStr.charAt(i) == '6') {	// 54
+				aArr[6]++;
+			} else if (aStr.charAt(i) == '7') {	// 55
+				aArr[7]++;
+			} else if (aStr.charAt(i) == '8') {	// 56
+				aArr[8]++;
+			} else if (aStr.charAt(i) == '9') {	// 57
+				aArr[9]++;
+			}
+		}
+		System.out.println("0~9의 갯수 : " + aArr[0] + "/" + aArr[1] + "/" + aArr[2] + "/" + 
+							aArr[3] + "/" + aArr[4] + "/" + aArr[5] + "/" + aArr[6] + "/" + 
+							aArr[7] + "/" + aArr[8] + "/" + aArr[9]);
+		
+		// +
+		// 이중for문, 형변환 사용
+		String bStr = "1/2/3/4/5/6/7/8/9/0/1/1/1/2/2/4/7/6";	// 초기값
+		int[] bArr = new int[10];								// 초기값
+		for (int i=0; i<bStr.length(); i++) {	// 자료형이 String인 bStr의 0번~마지막 인덱스 까지 반복
+			for (int j=0; j<=9; j++) {			// 0~9까지 반복
+				// 자료형이 String인 bStr의 문자열을 charAt메소드로 떼어난 문자가 '0'~'9'가 나오게 되는데,
+				// 문자 '0'~'9'는 10진수로 48~57의 값을 갖게 된다.
+				// 이때 정수인 j(0~9)와 문자 '0'을 합한다면 j+48의 값이 나오게 되므로 j가 0~9를 반복할 때 48~57의 값이 나온다.
+				// 나온 값인 48~57을 형변환을 통해 char로 바꿔준다면 '0'~'9'의 문자가 나오게 된다.
+				if (bStr.charAt(i) == (char)(j + '0')) {
+					bArr[j]++;					// bArr배열의 0~9번째 인덱스의 값을 +1 해준다. (bArr[j]++)
+				}
+			}
+		}
+		System.out.println("0~9의 갯수 : " + bArr[0] + "/" + bArr[1] + "/" + bArr[2] + "/" + 
+				bArr[3] + "/" + bArr[4] + "/" + bArr[5] + "/" + bArr[6] + "/" + 
+				bArr[7] + "/" + bArr[8] + "/" + bArr[9]);
+	}
+
+}
