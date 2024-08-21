9181fa97d3a43ac2a699c78b414fe3959b9e66fd
J1NU2 <pj1nu2@gmail.com>
2024-08-21 09:41:05 +0900

    Java 이중for문 미션

--- _05_이중for문/_0819_Q12.java
+++ _05_이중for문/_0819_Q12.java
@@ -15,7 +15,22 @@ public class _0819_Q12 {
 		 * 000*
 		 */
 		// 단, for문 2개만 사용하여 출력하시오. (if문의 수는 상관없음)
-		
+		int zeroCnt = -1;
+		for (int i=0; i<7; i++) {
+			if (i < 4) {
+				zeroCnt++;
+			} else {
+				zeroCnt--;
+			}
+			for (int j=0; j<4+zeroCnt; j++) {		
+				if (j > 2-zeroCnt) {
+					System.out.print("*");
+				} else {
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
