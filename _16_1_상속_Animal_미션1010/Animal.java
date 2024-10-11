cc0e2b3a370a916e61957f14347b1e78922ff27d
J1NU2 <pj1nu2@gmail.com>
2024-10-11 16:16:49 +0900

    Java 추상클래스/ 추상메서드

--- _16_1_상속_Animal_미션1010/Animal.java
+++ _16_1_상속_Animal_미션1010/Animal.java
@@ -1,12 +1,17 @@
 package _16_1_상속_Animal_미션1010;
 
 // 부모 클래스, Super Class
-public class Animal {
+// 추상 클래스, 추상 메서드가 있다면 객체를 만들 수 없다.
+public abstract class Animal {
 	// 동물에 대한 변수
 	public String zooName = "휴먼동물원";	// 동물원 이름
 	public String animalName = null;	// 동물 이름
 	public int age = 0;					// 나이
 	public int weight = 0;				// 몸무게
+	
+	// 추상 메서드, 구현되지 않은 상태
+	// 상속받으면 반드시 오버라이드(Override)를 해야 한다.
+	public abstract void prt();
 	
 	// 먹는 기능
 	public void prey() {
--- _16_1_상속_Animal_미션1010/Mongky.java
+++ _16_1_상속_Animal_미션1010/Mongky.java
@@ -2,6 +2,13 @@ package _16_1_상속_Animal_미션1010;
 
 // 자식 클래스, Sub Class
 public class Mongky extends Animal {	// 부모 클래스로 Animal 클래스를 상속 받는다.
+	// 부모 클래스, Super Class
+	// 상속받는 클래스에서 추상 메서드 선언이 된 메서드는 자식 클래스(Sub Class)에서 반드시 오버라이드 해야 한다.
+	@Override
+	public void prt() {
+		
+	}
+	
 	// 메서드 오버라이드를 통한 재정의
 	@Override
 	public void prey() {	// 먹는 기능
